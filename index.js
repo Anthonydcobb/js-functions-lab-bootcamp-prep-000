da73377d650cf90ae4a63850de6b711b05f1fb14
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-06 23:53:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const happyHolidays => return "Happy holidays!";
 const happyHolidaysTo = (name) => return `Happy holidays, ${name}`;
 
 const happyHolidayTo = (holiday, name) => return `Happy ${holiday}, ${name}`;
+
+const holidayCountdown = (holiday, days) => return `${days}`

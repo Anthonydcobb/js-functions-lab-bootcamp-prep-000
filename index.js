c8e0b346b68ac92dbc7ef3047218cbf7b2b730b5
Anthony David Cobb <anthonydcobb@gmail.com>
2019-10-07 00:03:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 let happyHolidays = () => {return "Happy holidays!"};
 
-let happyHolidaysTo = (name) => {return `Happy holidays, ${name}`};
+let happyHolidaysTo = (name) => {return `Happy holidays, ${name}.`};
 
-let happyHolidayTo = (holiday, name) => {return `Happy ${holiday}, ${name}`};
+let happyHolidayTo = (holiday, name) => {return `Happy ${holiday}, ${name}.`};
 
 let holidayCountdown = (holiday, days) => {return `It's ${days} days unitl ${holiday}!`};
 
